32355b5fdda3c5b63eb2021faf16916a990d0fa5
Gayashan Bombuwala <gayashanbc@gmail.com>
2017-04-07 22:45:52 +0530

    Initial commit
    Create interfaces for the Subject and the Observer

---
+++ Observer.hpp
@@ -0,0 +1,24 @@
+//
+// Created by shan on 4/7/17.
+//
+
+#ifndef OBSERVER_PATTERN_OBSERVER_HPP
+#define OBSERVER_PATTERN_OBSERVER_HPP
+
+
+class Observer {
+
+public:
+
+    /**
+     * Update the state of this observer
+     * @param temp new temperaure
+     * @param humidity new humidity
+     * @param pressure new pressure
+     */
+    virtual void update(float temp, float humidity, float pressure) = 0;
+
+};
+
+
+#endif //OBSERVER_PATTERN_OBSERVER_HPP
---
+++ Subject.hpp
@@ -0,0 +1,38 @@
+//
+// Created by shan on 4/7/17.
+//
+
+#ifndef OBSERVER_PATTERN_SUBJECT_HPP
+#define OBSERVER_PATTERN_SUBJECT_HPP
+
+
+#include "Observer.hpp"
+
+/**
+ * Interface for the Subject
+ */
+class Subject {
+
+public:
+
+    /**
+     * Register an observer
+     * @param observer the observer object to be registered
+     */
+    virtual void registerObserver(Observer observer) = 0;
+
+    /**
+     * Unregister an observer
+     * @param observer the observer object to be unregistered
+     */
+    virtual void removeObserver(Observer observer) = 0;
+
+    /**
+     * Notify all the registered observers when a change happens
+     */
+    virtual void notifyObserver() = 0;
+
+};
+
+
+#endif //OBSERVER_PATTERN_SUBJECT_HPP
